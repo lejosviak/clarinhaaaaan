3a30c81c6d32b1e0a2f4d021d8590585b60e1b89
lejosviak <163347879+lejosviak@users.noreply.github.com>
2024-09-23 12:25:21 -0300

    k

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
-let nomeusuario = "leticia"
-// alert ('nomeUsuario')
-function soma (num1, num2) {
-    return num1+num2 
+let nomeUsuario = 'leticia';
+// alert (nomeUsuario)
+function soma (num1, num2){
+return num1+num2}
+console.log(soma(4,2));
+
+function areaQuadrado (lado){
+    return lado**2;
 }
-console.log(soma (1,2));
+console.log (areaQuadrado)
+
+let areaTriangulo =
+function (base, altura) {
+    return (base*altura)/2;
+}
+console.log (areaTriangulo)
+
+let a = parseInt(prompt ("primeiro valor"))
+let b = parseInt(prompt ("segundo valor"))
+let c = parseInt(prompt ("terceiro valor"))
+
+function media(a,b,c) {
+    return((a+b+c)/3);
+}
+document.write(media(a, b, c))
